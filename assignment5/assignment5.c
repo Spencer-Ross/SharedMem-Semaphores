510e2e7c9e48f712caa63a2a36c4ae988fe8a5b7
Spencer <spencerthomasr@gmail.com>
2020-03-07 18:15:53 -0800

    init commit

--- assignment5/assignment5.c
+++ assignment5/assignment5.c
@@ -1,5 +1,25 @@
+/***********************************************************************
+name: Spencer Ross
+	assignment5 -- 
+description:	
+	
+***********************************************************************/
+
+/* Includes and definitions */
+#include <unistd.h>
+#include <sys/wait.h>
+#include <signal.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <errno.h>
+#include <string.h>
+#include "random.h"
+
+void handler(int nothing) {}
 
 int main(int argc, char *argv[]){
-	/* code */
+	struct sigaction sa;
+	memset(&sa, 0, NULL);
+	
 	return 0;
 }
